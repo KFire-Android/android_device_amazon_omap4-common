dc0492cab105ba8d663827db570d53bd4ab15708
Hashcode <hashcode0f@gmail.com>
2013-01-09 16:58:58 -0800

    fix check for otter board name

--- audio-otter/Android.mk
+++ audio-otter/Android.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(findstring otter, $TARGET_BOOTLOADER_BOARD_NAME)),otter)
+ifeq ($(findstring otter, $(TARGET_BOOTLOADER_BOARD_NAME)),otter)
 
 LOCAL_PATH := $(call my-dir)
 
