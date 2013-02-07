8fe15c9b3dc85de44ce0746862b8861cb413e94e
Hashcode <hashcode0f@gmail.com>
2013-02-07 01:14:03 -0800

    fix hdcp.keys symlink
    Change-Id: Id4616448f49032e2bb6845b81daeec748a27c297

--- Android.mk
+++ Android.mk
@@ -4,28 +4,25 @@ ifeq ($(TARGET_BOARD_PLATFORM),omap4)
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(TARGET_SIMULATOR),true)
+
 ifeq ($(BOARD_CREATE_AMAZON_HDCP_KEYS_SYMLINK), true)
-include $(CLEAR_VARS)
 
-LOCAL_MODULE := jem_hdcp_keys
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := FAKE
-LOCAL_MODULE_SUFFIX := -timestamp
+#Create HDCP symlink
+HDCP_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/hdcp.keys
+$(HDCP_SYMLINK): HDCP_KEYS_FILE := /efs/hdcp/hdcp.kek.wrapped
+$(HDCP_SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> $(HDCP_KEYS_FILE)"
+	@mkdir -p $(TARGET_OUT_VENDOR)/firmware
+	@rm -rf $@
+	$(hide) ln -fs $(HDCP_KEYS_FILE) $@
 
-include $(BUILD_SYSTEM)/base_rules.mk
+ALL_DEFAULT_INSTALLED_MODULES += $(HDCP_SYMLINK)
 
-$(LOCAL_BUILT_MODULE): HDCP_KEYS_FILE := /efs/hdcp/hdcp.kek.wrapped
-$(LOCAL_BUILT_MODULE): SYMLINK := $(TARGET_OUT_VENDOR)/firmware/hdcp.keys
-$(LOCAL_BUILT_MODULE): $(LOCAL_PATH)/Android.mk
-$(LOCAL_BUILT_MODULE):
-$(hide) echo "Symlink: $(SYMLINK) -> $(HDCP_KEYS_FILE)"
-	$(hide) mkdir -p $(dir $@)
-	$(hide) mkdir -p $(dir $(SYMLINK))
-	$(hide) rm -rf $@
-	$(hide) rm -rf $(SYMLINK)
-	$(hide) ln -sf $(HDCP_KEYS_FILE) $(SYMLINK)
-	$(hide) touch $@
+# for mm/mmm
+all_modules: $(HDCP_SYMLINK)
+
 endif
+
 include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
 
