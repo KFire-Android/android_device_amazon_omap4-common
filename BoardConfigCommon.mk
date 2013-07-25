57a5991aea008fd1aa4aae0c0954040b13716f64
Hashcode <hashcode0f@gmail.com>
2013-07-25 12:37:18 -0700

    add CPU_VARIANT + cleanup

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -42,14 +42,12 @@ BOARD_USE_TI_ENHANCED_DOMX := true
 
 # Processor
 TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_VARIANT := cortex-a9
 TARGET_CPU_ABI2 := armeabi
 TARGET_CPU_SMP := true
 TARGET_ARCH := arm
 TARGET_BOARD_PLATFORM := omap4
 TARGET_ARCH_VARIANT := armv7-a-neon
-ARCH_ARM_HAVE_TLS_REGISTER := true
-TARGET_GLOBAL_CFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
-TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a9 -mfpu=neon -mfloat-abi=softfp
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000
