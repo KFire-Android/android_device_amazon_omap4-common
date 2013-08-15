8c8c79c939ac9b4c329a11f76e1bf233a3b3ccb4
Hashcode <hashcode0f@gmail.com>
2013-08-15 12:27:20 -0700

    location fix: add gps.conf to system/etc
    Change-Id: Ib86ede8a9b1813337aab83e7d0423070ff9c288b

--- common.mk
+++ common.mk
@@ -115,5 +115,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libion.so
 
+# Prebuilts
+PRODUCT_COPY_FILES += \
+    $(COMMON_FOLDER)/prebuilt/etc/gps.conf:/system/etc/gps.conf
+
 $(call inherit-product-if-exists, vendor/amazon/omap4-common/omap4-common-vendor.mk)
 
