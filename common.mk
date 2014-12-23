e6f19a7773785c1ab31e1a272e9ef1f5a98f9cac
Hashcode <hashcode0f@gmail.com>
2014-12-23 11:36:58 -0800

    common: update common OMAP4 properties
    Change-Id: Ib36b8ebbd6bfc19e5a1a6b22ee7320f5b87999d1

--- common.mk
+++ common.mk
@@ -51,6 +51,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
     $(call add-to-product-copy-files-if-exists,packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:system/etc/permissions/android.software.live_wallpaper.xml)
 
+# Device settings
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0 \
     wifi.supplicant_scan_interval=120 \
@@ -58,10 +59,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
     com.ti.omap_enhancement=true \
     omap.enhancement=true \
     ro.crypto.state=unencrypted \
+    persist.lab126.chargeprotect=1 \
     persist.sys.usb.config=mtp,adb \
     persist.sys.root_access=3 \
     ro.bq.gpu_to_cpu_unsupported=1 \
-    media.stagefright.cache-params=18432/20480/15
+    media.stagefright.cache-params=18432/20480/15 \
+    ro.ksm.default=1 \
+    camera2.portability.force_api=1
 
 PRODUCT_CHARACTERISTICS := tablet,nosdcard
 
