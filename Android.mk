8c9ad3246bee1aa73d65e964fc64b6911700a15a
Hashcode <hashcode0f@gmail.com>
2015-02-06 08:51:28 -0800

    build: remove gralloc symlink for move to omap4-next
    Change-Id: Ic6c150068c78142fc74969a176f9d22d110eb26d

--- Android.mk
+++ Android.mk
@@ -39,22 +39,6 @@ all_modules: $(HDCP_SYMLINK)
 
 endif
 
-LOCAL_PATH := $(call my-dir)
-
-#Creating Gralloc SymLink
-GRALLOC_SYMLINK := $(TARGET_OUT_VENDOR)/lib/hw/gralloc.$(TARGET_BOARD_PLATFORM).so
-$(GRALLOC_SYMLINK): GRALLOC_FILE := gralloc.omap$(TARGET_BOARD_OMAP_CPU).so
-$(GRALLOC_SYMLINK): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
-	@echo "Symlink: $@ -> $(GRALLOC_FILE)"
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@rm -rf $@
-	$(hide) ln -fs $(GRALLOC_FILE) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(GRALLOC_SYMLINK)
-
-# for mm/mmm
-all_modules: $(GRALLOC_SYMLINK)
-
 include $(call first-makefiles-under,$(LOCAL_PATH))
 endif
 
