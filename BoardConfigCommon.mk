03d637a4e08567c105a918b05ee9bdb37a706ee6
Hashcode <hashcode0f@gmail.com>
2015-05-07 16:13:32 -0700

    omap4-common: sepolicy updates
    Removes most of the denials on otter that I was seeing.  I'll test on more
    devices as I get time and follow up with more changes if needed.
    
    Change-Id: I2289622eb2682c868cd648ec04cc5e7114100133

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -63,20 +63,26 @@ BOARD_SEPOLICY_DIRS += \
     $(COMMON_FOLDER)/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-    debuggered.te \
     bluetooth.te \
-    file_contexts \
+    debuggered.te \
     device.te \
     dhcp.te \
     domain.te \
     file.te \
+    file_contexts \
+    fixmac.te \
+    init.te \
     init_shell.te \
     mediaserver.te \
     netd.te \
     pvrsrvinit.te \
     rild.te \
+    setup_fs.te \
+    shell.te \
+    smc_pa.te \
     system.te \
     tee.te \
+    ueventd.te \
     vold.te
 
 # Include an expanded selection of fonts
