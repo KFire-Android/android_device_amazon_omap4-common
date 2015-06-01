30daaeaf3b893987efe3d9a03736cc86682356b9
Hashcode <hashcode0f@gmail.com>
2015-06-01 09:41:39 -0700

    omap4-common: treat all OMAP4 devices as low-ram via build.prop
    Change-Id: I7ed0b709fcc94c077740c0199a5eacef472aef05

--- common.mk
+++ common.mk
@@ -57,6 +57,15 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
 
+# Low-RAM optimizations
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.config.low_ram=true \
+    persist.sys.force_highendgfx=true \
+    dalvik.vm.jit.codecachesize=0 \
+    config.disable_atlas=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+
 # Device settings
 ADDITIONAL_BUILD_PROPERTIES += \
     wifi.interface=wlan0 \
